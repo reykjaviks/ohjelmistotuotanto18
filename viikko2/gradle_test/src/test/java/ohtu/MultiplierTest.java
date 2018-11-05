b7ddb52b8ab2d1a90ffa495e61346614a6ba47a4
Elizabeth Berg <elizabethinposti@gmail.com>
2018-11-05 19:51:10 +0200

    Fix exercise 2: test

--- viikko2/gradle_test/src/test/java/ohtu/MultiplierTest.java
+++ viikko2/gradle_test/src/test/java/ohtu/MultiplierTest.java
@@ -9,7 +9,7 @@ public class MultiplierTest {
     public void kertominenToimii() {
         Multiplier viisi = new Multiplier(5);
 
-        assertEquals(6, viisi.multipliedBy(1));
+        assertEquals(5, viisi.multipliedBy(1));
         assertEquals(35, viisi.multipliedBy(7));
     }
 
