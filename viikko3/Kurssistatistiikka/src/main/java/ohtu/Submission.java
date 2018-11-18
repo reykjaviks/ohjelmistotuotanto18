3076a4616692b6947f9f869451468d61d21cdb85
Elizabeth Berg <elizabethinposti@gmail.com>
2018-11-18 16:41:14 +0200

    Fix week 3: exercise 1

--- viikko3/Kurssistatistiikka/src/main/java/ohtu/Submission.java
+++ viikko3/Kurssistatistiikka/src/main/java/ohtu/Submission.java
@@ -1,56 +1,17 @@
 package ohtu;
 
-import java.util.stream.*;
 import java.util.Arrays;
 
 public class Submission {
-
     private int week;
     private int hours;
     private int[] exercises;
     private String course;
-    private int exerciseSum;
-
-    public void setWeek(int week) {
-        this.week = week;
-    }
-
-    public int getWeek() {
-        return week;
-    }
-
-    public void setHours(int hours) {
-      this.hours = hours;
-    }
-
-    public int getHours() {
-      return this.hours;
-    }
-
-    public int countExercises() {
-        return IntStream.of(this.exercises).sum();
-    }
-
-    public void setExercises(int[] exercises) {
-      this.exercises = exercises;
-    }
-
-    public int[] getExercises() {
-      return this.exercises;
-    }
-
-    public void setCourse(String course) {
-      this.course = course;
-    }
-
-    public String getCourse() {
-      return this.course;
-    }
 
     @Override
     public String toString() {
-        return "" + this.course + ", viikko " + this.week +
-                " tehtyjä tehtäviä yhteensä " + this.countExercises() +
+        return "" + this.course + ", viikko " + this.week + ":\n" +
+                " tehtyjä tehtäviä yhteensä " + this.exercises.length +
                 " aikaa kului " + this.hours + " tehdyt tehtävät " +
                 Arrays.toString(this.exercises);
     }
