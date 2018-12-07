6d6ee3e7aa916867426e74c328e2a9e45c7bd34c
Elizabeth Berg <elizabethinposti@gmail.com>
2018-12-07 15:33:12 +0200

    Add Or.java

--- viikko6/QueryLanguage/src/main/java/statistics/Main.java
+++ viikko6/QueryLanguage/src/main/java/statistics/Main.java
@@ -5,13 +5,20 @@ import statistics.matcher.*;
 public class Main {
     public static void main(String[] args) {
         Statistics stats = new Statistics(new PlayerReaderImpl("http://nhlstats-2013-14.herokuapp.com/players.txt"));
-          
-        Matcher m = new And( new HasAtLeast(10, "goals"),
+
+
+        // Matcherit
+        Matcher and = new And( new HasAtLeast(10, "goals"),
                              new HasAtLeast(10, "assists"),
                              new PlaysIn("PHI")
         );
-        
-        for (Player player : stats.matches(m)) {
+
+        Matcher or = new Or( new HasAtLeast(40, "goals"),
+                             new HasAtLeast(60, "assists"),
+                             new HasAtLeast(85, "points")
+        );
+
+        for (Player player : stats.matches(or)) {
             System.out.println( player );
         }
     }
